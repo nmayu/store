399ebdd08c893c70ea3b0d3505e1e1b5d57bce3e
mayumi negishi <81173000+nmayu@users.noreply.github.com>
2021-06-20 08:57:30 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+$(function(){
+    $(`.toggle`).on(`click`,function(){
+        if($(`#header`).hasClass(`open`)){
+            $(`#header`).removeClass(`open`);
+        } else {
+            $(`#header`).addClass("open");
+        }
+    });
+
+    $(`#mask`).on(`click`,function(){
+        $(`#header`).removeClass(`open`);
+    });
+});
